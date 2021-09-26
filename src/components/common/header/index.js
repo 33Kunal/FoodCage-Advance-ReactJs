7171b2aa55ec6c52ddface23a770beeebc45b653
kshuvam <shuvam.kumar@groww.in>
2021-09-26 16:33:52 +0530

    Completed header

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -10,13 +10,33 @@ const Header = () => {
         className="header-logo"
       />
       <div className="header-right">
-        <div className="header-location-container">
-          <div>THis is location</div>
+        <div className="header-location-search-container">
+          <div className="location-wrapper">
+            <div className="location-icon-name">
+              <i className="fi fi-rr-marker absolute-center location-icon"></i>
+              <div>Bangalore</div>
+            </div>
+            <i className="fi fi-rr-caret-down absolute-center"></i>
+          </div>
           <div className="location-search-separator"></div>
-          <div>This is search</div>
+          <div className="header-searchBar">
+            <i className="fi fi-rr-search absolute-center search-icon"></i>
+            <input
+              className="search-input"
+              placeholder="Search for restaurant, cuisine or a dish"
+            />
+          </div>
         </div>
 
-        <div>THis is profile</div>
+        <div className="profile-wrapper">
+          <img
+            src="https://b.zmtcdn.com/images/user_avatars/mug_2x.png?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A"
+            className="header-profile-image"
+            alt="Profile"
+          />
+          <span className="header-username">Shuvam</span>
+          <i className="fi fi-rr-angle-small-down absolute-center profile-options-icon"></i>
+        </div>
       </div>
     </div>
   );
